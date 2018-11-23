668e6361e08fe1bb0596769125946fab67b2b778
Maciej Głowiński <maciej.glowinski@comarch.pl>
2018-11-23 18:33:43 +0100

    add book car controller

---
+++ src/main/java/com/xing/gccars/controller/BookCarController.java
@@ -0,0 +1,10 @@
+package com.xing.gccars.controller;
+
+import com.xing.gccars.service.CarService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookCarController {
+
+    private CarService carService;
+}
