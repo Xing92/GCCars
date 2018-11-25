6bf824032b60322a324aaf037d6fd08d847ff5cd
mglowinski <mck00@o2.pl>
2018-11-25 01:28:26 +0100

    add service annotation

--- src/main/java/com/xing/gccars/service/BorrowedDateServiceImpl.java
+++ src/main/java/com/xing/gccars/service/BorrowedDateServiceImpl.java
@@ -2,9 +2,11 @@ package com.xing.gccars.service;
 
 import com.xing.gccars.repository.BorrowedDateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
 
+@Service
 public class BorrowedDateServiceImpl implements BorrowedDateService {
 
     private final BorrowedDateRepository borrowedDateRepository;
