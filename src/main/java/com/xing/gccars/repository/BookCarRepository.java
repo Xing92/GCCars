9d02f1011c5828c2a46ad5730189724fb2d4ae91
mglowinski <mck00@o2.pl>
2018-11-27 00:34:34 +0100

    fix query for check is car is available

--- src/main/java/com/xing/gccars/repository/BookCarRepository.java
+++ src/main/java/com/xing/gccars/repository/BookCarRepository.java
@@ -9,17 +9,13 @@ import org.springframework.stereotype.Repository;
 import java.util.Calendar;
 
 @Repository
-public interface BorrowedDateRepository extends JpaRepository<BorrowedDate, Long> {
+public interface BookCarRepository extends JpaRepository<BorrowedDate, Long> {
 
     @Query("select case when count(b) = 0 then true else false end " +
             "from BorrowedDate as b " +
-            "where :startDate not between b.startDate and b.endDate " +
-            "and :endDate not between b.startDate and b.endDate " +
-            "and b.car.id = :carId " +
-            "and b.car.id NOT IN (select DISTINCT bd.car.id " +
-            "from BorrowedDate bd " +
-            "where :startDate between bd.startDate and bd.endDate " +
-            "OR :endDate between bd.startDate and bd.endDate)")
+            "where b.car.id = :carId and (" +
+            ":startDate between b.startDate and b.endDate " +
+            "or :endDate between b.startDate and b.endDate) ")
     boolean checkAvailabilityCarById(@Param("startDate") Calendar startDate,
                                      @Param("endDate") Calendar endDate,
                                      @Param("carId") Long id);
