83661eeadb397385ccc8bddf34a1285e8a5054e5
mglowinski <mck00@o2.pl>
2018-11-27 00:33:08 +0100

    rename services

--- src/main/java/com/xing/gccars/service/BookCarServiceImpl.java
+++ src/main/java/com/xing/gccars/service/BookCarServiceImpl.java
@@ -1,31 +1,31 @@
 package com.xing.gccars.service;
 
 import com.xing.gccars.model.BorrowedDate;
-import com.xing.gccars.repository.BorrowedDateRepository;
+import com.xing.gccars.repository.BookCarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
 
 @Service
-public class BorrowedDateServiceImpl implements BorrowedDateService {
+public class BookCarServiceImpl implements BookCarService {
 
-    private final BorrowedDateRepository borrowedDateRepository;
+    private final BookCarRepository bookCarRepository;
 
     @Autowired
-    public BorrowedDateServiceImpl(BorrowedDateRepository borrowedDateRepository) {
-        this.borrowedDateRepository = borrowedDateRepository;
+    public BookCarServiceImpl(BookCarRepository bookCarRepository) {
+        this.bookCarRepository = bookCarRepository;
     }
 
     @Override
     public boolean checkAvailabilityCarById(Calendar startDate,
                                             Calendar endDate,
                                             Long id) {
-        return borrowedDateRepository.checkAvailabilityCarById(startDate, endDate, id);
+        return bookCarRepository.checkAvailabilityCarById(startDate, endDate, id);
     }
 
     @Override
     public BorrowedDate addBorrowedDate(BorrowedDate borrowedDate) {
-        return borrowedDateRepository.save(borrowedDate);
+        return bookCarRepository.save(borrowedDate);
     }
 }
